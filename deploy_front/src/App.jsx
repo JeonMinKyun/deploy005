8b0549573e89a5d148eed50caee717ca1c143bc0
lucas0429 <lucas0429@gmail.com>
2026-02-11 12:48:27 +0900

    feet : 프론트

---
+++ deploy_front/src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect (() =>{
+    fetch("http://localhost:8080/")
+    .then((response => response.text()))
+    .then(data =>{
+      setMessage(data)
+    })
+    .catch((error)=>{
+      console.log("에러발생:"+error );
+    })
+  }
+)
+  return (
+    <>
+      <div style={{padding:'40px', alignContent:'center'}}>
+        <h3>Spring Boot 서버 응답 결과</h3>
+        <h4>{message}</h4>
+      </div>
+    </>
+  )
+}
+
+export default App
