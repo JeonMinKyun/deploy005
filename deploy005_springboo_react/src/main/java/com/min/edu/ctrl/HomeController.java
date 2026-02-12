1214ef4a6ef5dc394e7bf4a1a948836e67f64f5f
lucas0429 <lucas0429@gmail.com>
2026-02-12 13:12:11 +0900

    feet : ngrok 주소요 CROS 허용

--- deploy005_springboo_react/src/main/java/com/min/edu/ctrl/HomeController.java
+++ deploy005_springboo_react/src/main/java/com/min/edu/ctrl/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://053c-1-11-163-250.ngrok-free.app")
 public class HomeController {
 
 	@GetMapping
